ed3eb13b6e3e802f3ccdbf023087b4d0dcf3fade
hp <bhagatnicky1234@gmail.com>
2024-12-24 17:33:57 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data,setdata]=useState("nicky")
+  function updatedata(){
+    setdata("ajay")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
+      <button onClick={updatedata}>click me</button>
     </div>
   );
 }
